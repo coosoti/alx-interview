2d36d1329631f21554ce004a84158f07f2e9203b
coosoti <charlesosoti@gmail.com>
2021-12-02 14:16:48 +0300

    [interview practice question] 2d array centric question - You have n number of locked boxes in front of you. Each box is numbered sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
    Write a method that determines if all the boxes can be opened.

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,29 @@
+#!usr/bin/python3
+"""
+this module contains a method that determines if all the boxes can be opened
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    determines if all boxes can be opened
+    @boxes: list of lists
+    Returns: True or False
+    """
+    n = len(boxes)
+    stack = [0]
+    unlocked_box = [1] + [0] * (n - 1)
+    i = 0
+
+    if n == 0:
+        return True
+    while stack:
+        j = stack.pop()
+        for index in boxes[j]:
+            if index > 0 and index < n and unlocked_box[index] == 0:
+                unlocked_box[index] = 1
+                stack.append(index)
+        i = i + 1
+    if 0 in unlocked_box:
+        return False
+    return True
